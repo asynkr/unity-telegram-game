1c0d21bed124061286bd755b3bc30b840a5987c8
AlexLaval <alex.laval.g@gmail.com>
2022-07-08 11:57:56 +0200

    🔥(scripts) remove unnecessary imports

--- Runtime/Scripts/Networking/ConnexionManager.cs
+++ Runtime/Scripts/Networking/ConnexionManager.cs
@@ -1,6 +1,4 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 using Asynkrone.UnityTelegramGame.Security;
--- Runtime/Scripts/Security/IObfuscation.cs
+++ Runtime/Scripts/Security/IObfuscation.cs
@@ -1,6 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 namespace Asynkrone.UnityTelegramGame.Security
 {
     public interface IObfuscation {
